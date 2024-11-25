98a82a059183f5be4c99f17e8cceead7b253e3d1
nayeon.kim <nayeon.kim@ucalgary.ca>
2024-11-25 09:48:16 -0700

    basic set up of pages

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,16 @@
+// src/LandingPage.js
+import React from 'react';
+import '../Styles/LandingPage.css'; 
+
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <header>
+        <h1>Welcome to the landing page</h1>
+      
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
