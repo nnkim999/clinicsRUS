245deb8d6b9bbc0ce3086f71b6a27847c14e6802
nadamrashad <129803692+nadamrashad@users.noreply.github.com>
2024-11-29 11:31:07 -0700

    ManageAppointment Updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 import LandingPage from './Pages/LandingPage';
 import BookAppointment from './Pages/BookAppointment';
 import AppointmentDetails from './Pages/AppointmentDetails';
+import ManageAppointment from './Pages/ManageAppointment';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<LandingPage />} /> {/* Default landing page */}
           <Route path="/book-appointment" element={<BookAppointment />} /> {/* Route to BookAppointment */}
           <Route path="/appointment-details" element={<AppointmentDetails />} /> {/* Route to BookAppointment */}
+          <Route path="/manage-appointment" element={<ManageAppointment />} />
         </Routes>
       </div>
     </Router>
